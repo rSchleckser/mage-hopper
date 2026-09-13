7ac36d0edaff6b9a10bcb12938408b6ffbd8e102
Richard D Schleckser <47959292+rSchleckser@users.noreply.github.com>
2026-09-13 08:49:13 -0700

    Update main.js
    testing hitbox

--- src/main.js
+++ src/main.js
@@ -28,7 +28,7 @@ const config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 300 }, // Set gravity
-      debug: false, // Set to true to see physics bodies
+      debug: true, // Set to true to see physics bodies
     },
   },
   scene: [menuScene, characterSelectScene, gameScene, pauseScene, gameOverScene, levelWinScene, gameWinScene],
